43b2a72205bb22a442eaae84aa21c1c6bd310bd8
Artem <scheglov1997@yandex.ru>
2020-09-19 20:19:36 +0300

    Add

--- MyFirstProject/urls.py
+++ MyFirstProject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home),
-    path('admin/', admin.site.urls),
+    path('reverse/', views.reverse, name='reverse'),
 
 ]
--- MyFirstProject/views.py
+++ MyFirstProject/views.py
@@ -1,9 +1,12 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
-def artem(request):
-    return HttpResponse('Hello Ar!!!m. How are you?')
-
+def reverse(request):
+	user_text = request.GET['usertext']
+	words = user_text.split()
+	number_of_words = len(words)
+	reversed_text = user_text[::-1]
+	return render(request, 'reverse.html', {'usertext':user_text,
+		'reversedtext':reversed_text, 'number_of_words':number_of_words})
 def home(request):
     return render(request, 'home.html', {'greeting' : 'hello'})
